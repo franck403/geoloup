759cecb62e92d5e66d537e950ee7c3d69f03f6e3
franck403 <79372051+franck403@users.noreply.github.com>
2023-07-23 19:25:26 -0400

    created script for the navbar

---
+++ navbar.js
@@ -0,0 +1,31 @@
+link = {
+    "accueil": "",
+    "project": "",
+    "blog": "",
+}
+links = [["https://geoloup.com/blog", "blog"], ["https://geoloup.com/", "accueil"], ["https://geoloup.com/project", "project"],]
+
+links.array.forEach(element => {
+    if (element[0] == document.URL) {
+        link[element[1]] == "active"
+    }
+});
+preview = `
+<ul>
+<li class="${link["accueil"]}">
+    <a href="https://geoloup.com/">
+        Accueil
+    </a>
+</li>
+<li  class="${link["project"]}">
+    <a href="https://geoloup.com/project">
+        Project
+    </a>
+</li>
+<li  class="${link["blog"]}">
+    <a href="https://geoloup.com/blog">
+        Blog
+    </a>
+</li>
+</ul>`
+document.getElementById("NavBar").innerHTML = preview
